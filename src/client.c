2c18c431e48567885ea5bcbd1f89c648fba5fc0d
Roxane RAFFI KHANSARI <rraffi-k@paul-f4Br6s1.clusters.42paris.fr>
2023-06-13 18:38:37 +0200

    BUSEEEEEEEEEEEE

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: rraffi-k <rraffi-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 11:27:54 by rraffi-k          #+#    #+#             */
-/*   Updated: 2023/06/13 15:29:09 by rraffi-k         ###   ########.fr       */
+/*   Updated: 2023/06/13 18:36:16 by rraffi-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,55 +15,94 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int	ft_atoi(const char *nptr)
-{
-	int	res;
-	int	i;
-	int	n;
-
-	i = 0;
-	n = 1;
-	res = 0;
-	while (nptr[i] && (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13)))
-		i++;
-	if (nptr[i] == '+')
-		i++;
-	else if (nptr[i] == '-')
-	{
-		n = -n;
-		i++;
-	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		res *= 10;
-		res = res + (nptr[i] - '0');
-		i++;
-	}
-	return (n * res);
-}
+int bit_received;
 
 static void handle_signals(int signal)
 {
 	if (signal == SIGUSR2)
-		ft_printf("SIGUSR2 recu");
+	{
+		// ft_printf("SIGUSR2 recu\n");
+		bit_received = 1;
+		// ft_printf("bit received value : %d\n", bit_received);
+	}
 }
 
-void send_message(pid_t server_pid)
+void send_message(char c, pid_t server_pid)
 {
-	kill(server_pid, SIGUSR1);
+	int	bit;
+
+	bit = 0;
+	// ft_printf("bit received value : %d\n", bit_received);
+	
+	while (bit < 8)
+	{
+		if ((c & (1 << bit)))
+		{	
+			if (kill(server_pid, SIGUSR1) < 0)
+				exit (0);
+		}
+		else
+		{	
+			if (kill(server_pid, SIGUSR2) < 0)
+				exit (0);			
+		}
+		bit++;
+		bit_received = 0;
+		while (!bit_received);
+		// usleep(100);
+	}
 }
 
 int main(int argc, char **argv)
 {
 	struct sigaction sig;
-	char	*buffer;
+	int i;
 
-	sig.sa_handler = &handle_signals;
+	bit_received = 1;
+	if (argc != 3)
+	{
+		ft_printf("Invalid number of arguments");
+		return (0);
+	}
+	ft_bzero(&sig, sizeof(sig));
+	sig.sa_handler = handle_signals;
 	sigaction(SIGUSR1, &sig, NULL);
 	sigaction(SIGUSR2, &sig, NULL);
-
-	send_message(ft_atoi(argv[1]));
+	i = 0;
+	while (argv[2][i])
+	{
+		send_message(argv[2][i], ft_atoi(argv[1]));
+		i++;
+	}
 	while (1)
 		pause();
 	return (0);
 }
+
+
+////////////////////////////////////////////////////////////
+
+
+// static void action(int signal)
+// {
+// 	if (signal == SIGUSR2)
+// 		ft_printf("SIGUSR2 recu");
+// 	else
+// 		ft_printf("SIGUSR2 recu");
+// }
+
+// void send_message(pid_t server_pid)
+// {
+// 	kill(server_pid, SIGUSR1);
+// 	usleep(100);
+// }
+
+// int main(int argc, char **argv)
+// {
+// 	signal(SIGUSR1, action);
+// 	signal(SIGUSR2, action);
+// 	send_message(ft_atoi(argv[1]));
+// 	while (1)
+// 		pause();
+// 	return (0);
+// }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: rraffi-k <rraffi-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 11:14:23 by rraffi-k          #+#    #+#             */
-/*   Updated: 2023/06/13 15:28:58 by rraffi-k         ###   ########.fr       */
+/*   Updated: 2023/06/13 18:35:56 by rraffi-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,25 @@
 
 static void handle_signals(int signal, siginfo_t *info, void *context)
 {
+	static int	bit;
+	static int	c;
+
+	(void) context;
 	if (signal == SIGUSR1)
-		ft_printf("SIGUSR1 recu");
+		c |= (0x01 << bit);
+	// else if (signal == SIGUSR1)
+	// 	c &= ~(0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		// usleep(100);
+		// ft_printf("on envoie SIGUSR2\n");
+		// if (kill(info->si_pid, SIGUSR2) < 0)
+		// 	exit(0);
+		ft_printf("%c", c);
+		c = 0;
+		bit = 0;
+	}
 	kill(info->si_pid, SIGUSR2);
 }
 
@@ -23,11 +40,50 @@ int	main(int argc, char **argv)
 {
 	struct sigaction sig;
 
+	// if (argc != 1)
+	// {
+	// 	ft_printf("Error\n");
+	// 	return (1);
+	// }
+	sig.sa_flags = SA_SIGINFO;
 	ft_printf("PID : %d", getpid());
-	sig.sa_sigaction = &handle_signals;
+	// (void) argc;
+	// (void) argv;
+	// ft_bzero(&sig, sizeof(sig));
+	sig.sa_sigaction = handle_signals;
 	sigaction(SIGUSR1, &sig, NULL);
 	sigaction(SIGUSR2, &sig, NULL);
 	while (1)
 		pause();
 }
+
+
+
+// static void handle_signals(int signal, siginfo_t *info, void *context)
+// {
+// 	static pid_t client_pid;
+
+// 	(void) context;
+// 	if (!client_pid)
+// 		client_pid = info->si_pid;
+// 	if (signal == SIGUSR1)
+// 		ft_printf("SIGUSR1 recu");
+// 	kill(client_pid, SIGUSR2);
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	struct sigaction sig;
+
+// 	sig.sa_flags = SA_SIGINFO;
+// 	ft_printf("PID : %d", getpid());
+// 	sig.sa_sigaction = handle_signals;
+// 	sigaction(SIGUSR1, &sig, NULL);
+// 	sigaction(SIGUSR2, &sig, NULL);
+// 	// signal(SIGUSR1, handle_signals);
+// 	// signal(SIGUSR2, handle_signals);
+// 	while (1)
+// 		pause();
+// 	return (0);
+// }
 
